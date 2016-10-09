ee8055afa4f6b1fd968ca83e5263cf0cc97cc990
Juan Carlos Perez <carlos@jcarlosperez.me>
2016-10-09 01:15:39 -0400

    Initial Commit

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Juan Carlos Perez on 10/9/16.
+//  Copyright © 2016 CP Digital Darkroom. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
